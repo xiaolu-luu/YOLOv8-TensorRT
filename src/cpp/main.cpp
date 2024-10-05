0779950b6d1311cf74d6b6b1f10b05ca60574717
xiaolu-luu <82944479+xiaolu-luu@users.noreply.github.com>
2024-10-05 23:31:14 +0800

    run-pose

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -8,13 +8,13 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /*这么实现目的在于让调用的整个过程精简化*/
-    string onnxPath    = "models/onnx/yolov8n.onnx";
+    string onnxPath    = "models/onnx/yolov8n-pose.onnx";
 
     auto level         = logger::Level::VERB;
     auto params        = model::Params();
 
     params.img         = {640, 640, 3};
-    params.task        = model::task_type::DETECTION;
+    params.task        = model::task_type::POSETASK;
     params.dev         = model::device::GPU;
     params.prec        = model::precision::FP32;
 
@@ -23,9 +23,9 @@ int main(int argc, char const *argv[])
 
     // 根据worker中的task类型进行推理
     worker->inference("data/source/car.jpg");
-    // worker->inference("data/source/airport.jpg");
-    // worker->inference("data/source/crossroad.jpg");
-    // worker->inference("data/source/bedroom.jpg");
+    worker->inference("data/source/airport.jpg");
+    worker->inference("data/source/crossroad.jpg");
+    worker->inference("data/source/bedroom.jpg");
 
     return 0;
 }
